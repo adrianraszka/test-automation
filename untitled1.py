a2903b58ff11279e956f2d8fd4275d744be4a4dd
adrianraszka <43697407+adrianraszka@users.noreply.github.com>
2019-04-03 16:35:53 +0200

    Add files via upload
    Inputing specific password:
    Enter character a from your Password:
    Enter character b from your Password:
    Enter character c from your Password:

---
+++ untitled1.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+import time
+
+# driver = webdriver.chrome.webdriver.WebDriver('C:\\Users\\P10481111\\Desktop\\untitled1\\chromedriver')
+#
+# driver.get()
+# driver.find_element_by_id("cookieChoiceDismiss").click()
+# driver.find_element_by_link_text('Login').click()
+#
+# login = driver.find_element_by_class_name('textfield')
+# login.send_keys("Pensioner8Unilever")
+# login.send_keys(Keys.ENTER)
+
+
+# y = []
+# x = ("s strij tisrj tisjr it 3akos dkao sd")
+# for letter in x:
+#     y.append(letter)
+#     if
+
+# colors = ["red", "green", "blue", "purple"]
+# for i in range(len(colors)):
+#     print(colors[i])
+
+# a = ["1", "2", "3", "a", "10"]
+# for i in range(len(a)):
+#     if a[i].isDigit() is true:
+#         print(index)
+
+
+# TODO: password as value of label field
+
+password1 = ("Enter character 10 from your Password:")
+password2 = ("Enter character 5 from your Password:")
+password3 = ("Enter character 8 from your Password:")
+
+
+def password_to_array(password):
+    password_name = ()
+    password_array = []
+
+    for letter in password:
+        password_array.append(letter)
+        print(password_array[17])
+
+
+
+
+from selenium.webdriver.common.keys import Keys
+
+password_to_array(password1)
+
+# for i in range(len(password)):
+#     #     if password_array[17] == 0:
+#     #         print(password_name[9])
+# elif str(i) == password_array[16]:
+#     print(password_name[i-1])
+
+# else:
+#     pass
+# str(i) == password_array[16]:
+# if str(i) == 0:
+#     print(i)
